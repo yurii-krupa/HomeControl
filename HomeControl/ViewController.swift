1f5c936c65a1a989f2d380cede59c8ebdb2014b3
Yurii Krupa <yurii.krupa.dev@gmail.com>
2018-12-12 10:24:56 +0200

    Initial Commit

---
+++ HomeControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeControl
+//
+//  Created by Yurii Krupa on 12/12/18.
+//  Copyright © 2018 Yurii Krupa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
