a636a0e9cd80768f0ca5bf100c21a80163d937e6
Yurii Krupa <yurii.krupa.dev@gmail.com>
2018-12-14 16:05:54 +0200

    first commit

---
+++ HomeControl/DeviceDetailedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DeviceDetailedViewController.swift
+//  HomeControl
+//
+//  Created by Yurii Krupa on 12/12/18.
+//  Copyright © 2018 Yurii Krupa. All rights reserved.
+//
+
+import UIKit
+
+class DeviceDetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HomeControl/DeviceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DeviceTableViewCell.swift
+//  HomeControl
+//
+//  Created by Yurii Krupa on 12/12/18.
+//  Copyright © 2018 Yurii Krupa. All rights reserved.
+//
+
+import UIKit
+
+class DeviceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
